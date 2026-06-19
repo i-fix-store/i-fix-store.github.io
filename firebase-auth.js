f919784af9050594109fe9f84ed583d825126182
ifix <137706522+i-fix-store@users.noreply.github.com>
2026-06-19 12:38:19 +0300

    Add files via upload

---
+++ firebase-auth.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE2-qaGOOlmhn9QA01J5wizJ_CcMMh7qE",
+  authDomain: "ifix-store-eecd9.firebaseapp.com",
+  projectId: "ifix-store-eecd9",
+  storageBucket: "ifix-store-eecd9.firebasestorage.app",
+  messagingSenderId: "862863699996",
+  appId: "1:862863699996:web:7af653f4de7d0baa0fafd9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+  const provider = new GoogleAuthProvider();
+
+  const result = await signInWithPopup(auth, provider);
+
+  const user = result.user;
+
+  await setDoc(
+    doc(db, "users", user.uid),
+    {
+      name: user.displayName,
+      email: user.email,
+      createdAt: new Date()
+    },
+    { merge: true }
+  );
+
+  document.getElementById("userName").innerHTML =
+    "مرحباً " + user.displayName + " 👋";
+
+  loginBtn.style.display = "none";
+});
